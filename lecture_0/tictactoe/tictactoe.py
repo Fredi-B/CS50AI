75107092c04fd29d3147ca999a5c67bf8ae272ef
Fredi-B <fredibechtoldt@outlook.com>
2024-09-05 20:52:11 +0200

    add comments and some cosmetics

--- lecture_0/tictactoe/tictactoe.py
+++ lecture_0/tictactoe/tictactoe.py
@@ -44,6 +44,8 @@ def actions(board):
     for row in board:
         j = 0
         for cell in row:
+
+            # Add empty cells to actions 
             if cell is EMPTY:
                 actions_set.add((i, j))
             j += 1
@@ -55,6 +57,7 @@ def result(board, action):
     """
     Returns the board that results from making move (i, j) on the board.
     """
+    # Deepcopy needed for recursive minimax()
     new_board = copy.deepcopy(board)
     i, j = action
     if i < 0 or j < 0:
@@ -70,17 +73,25 @@ def winner(board):
     """
     Returns the winner of the game, if there is one.
     """
+    # List of functions to check if there is a winner
     check_functions = [check_columns, check_rows, check_diagonals]
     for check in check_functions:
-        ret_val = check(board)
-        if ret_val is not None:
-            return ret_val
+        player_wins = check(board)
+
+        # Return winner
+        if player_wins is not None:
+            return player_wins
     return None
 
 
 def check_rows(board):
+    '''
+    Returns winner if won horizontally
+    '''
     for row in board:
         for i in range(len(row) - 1):
+
+            # Check if the current and the next cell are not empty and equal
             if row[i] is EMPTY or row[i] != row[i + 1]:
                 break
             if i == 1:
@@ -89,21 +100,40 @@ def check_rows(board):
 
 
 def check_columns(board):
+    '''
+    Returns winner if won vertically
+    '''
     len_cols = len(board[0])
     len_rows = len(board)
     for i in range(len_cols):
+
+        # Inner loop loops through the rows ('j' in the first '[]')
         for j in range(len_rows - 1):
+
+            # Check if the current and the next cell are not empty and equal
             if board[j][i] is EMPTY or board[j][i] != board[j + 1][i]:
                 break
+
+            # Check if there a three vertically
             if j == 1:
+
+                # Return the column 'i'
                 return board[0][i]
     return None
 
 
 def check_diagonals(board):
+    '''
+    Returns winner if won diagonally
+    '''
+    # Check top left to bottom right diagonal
     if board[0][0] is not EMPTY:
+
+        # Sets do not have duplicates. If there a three 'X' the length will be 1.
         if len(set([board[0][0], board[1][1], board[2][2]])) == 1:
             return board[0][0]
+
+    # Check bottom left to top right diagonal
     if board[2][0] is not EMPTY:
         if len(set([board[2][0], board[1][1], board[0][2]])) == 1:
             return board[2][0]
@@ -136,6 +166,8 @@ def minimax(board):
     Returns the optimal action for the current player on the board.
     """
     if player(board) == X:
+
+        # Only store the second return value
         _, action = max_value(board)
     else:
         _, action = min_value(board)
@@ -147,7 +179,6 @@ def max_value(board):
     Recursive function.
     Returns the value and the optimal action of a board.
     '''
-
     # Return value of terminal board
     if terminal(board):
         return utility(board), None
@@ -158,7 +189,7 @@ def max_value(board):
     # Loop through all possible actions
     actions_set = actions(board)
     for action in actions_set:
-        v_tmp,_ = min_value(result(board, action))
+        v_tmp, _ = min_value(result(board, action))
 
         # Store the best action
         if v_tmp > v:
@@ -184,7 +215,7 @@ def min_value(board):
     # Loop through all possible actions
     actions_set = actions(board)
     for action in actions_set:
-        v_tmp,_ = max_value(result(board, action))
+        v_tmp, _ = max_value(result(board, action))
 
         # Store the best action
         if v_tmp < v:
